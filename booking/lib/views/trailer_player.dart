c3bd117dbc21dcdae17a70a221402dedd432ccf4
HamzaRekik <rekikhamza11@gmail.com>
2023-12-06 03:18:59 +0100

    fixed trailer

--- booking/lib/views/trailer_player.dart
+++ booking/lib/views/trailer_player.dart
@@ -1,7 +1,8 @@
-import 'package:booking/services/movies_service.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:dio/dio.dart';
+import 'package:booking/services/movies_service.dart';
 
 class MyVideoPlayer extends StatefulWidget {
   MyVideoPlayer({required this.movieID});
@@ -13,51 +14,59 @@ class MyVideoPlayer extends StatefulWidget {
 }
 
 class _MyVideoPlayerState extends State<MyVideoPlayer> {
-  late final YoutubePlayerController _controller;
+  late YoutubePlayerController _controller;
+
   Future<String> getMovieTrailer() async {
-    var videoKey =
-        await MoviesService(Dio()).getTrailer(movieID: widget.movieID);
-
-    return videoKey;
-  }
-
-  init() async {
     try {
-      final key = await getMovieTrailer();
-      _controller = YoutubePlayerController(
-        initialVideoId: key,
-        flags: const YoutubePlayerFlags(
-          hideThumbnail: true,
-          autoPlay: false,
-          mute: false,
-        ),
-      );
-    } on Exception catch (e) {
-      print(e);
+      var videoKey =
+          await MoviesService(Dio()).getTrailer(movieID: widget.movieID);
+
+      return videoKey;
+    } catch (e) {
+      print("Error fetching trailer: $e");
+      return "";
     }
   }
 
   @override
-  void initState() {
-    super.initState();
-
-    init();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return YoutubePlayerBuilder(
-      player: YoutubePlayer(
-        controller: _controller,
-        showVideoProgressIndicator: true,
-        progressIndicatorColor: Colors.deepPurple,
-        // onReady: () {
-        //   _controller.toggleFullScreenMode();
-        // },
-        // onEnded: (metaData) => Navigator.pop(context),
-      ),
-      builder: (context, player) {
-        return player;
+    return FutureBuilder<String>(
+      future: getMovieTrailer(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          _controller = YoutubePlayerController(
+            initialVideoId: snapshot.data ?? '',
+            flags: const YoutubePlayerFlags(
+              hideThumbnail: true,
+              autoPlay: false,
+              mute: false,
+            ),
+          );
+
+          return YoutubePlayerBuilder(
+            player: YoutubePlayer(
+              onReady: () => SystemChrome.setEnabledSystemUIMode(
+                  SystemUiMode.manual,
+                  overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]),
+              onEnded: (metaData) {
+                _controller.toggleFullScreenMode();
+                SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+                    overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+              },
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.deepPurple,
+            ),
+            builder: (context, player) {
+              return player;
+            },
+          );
+        } else {
+          return Center(
+              child: CircularProgressIndicator(
+            color: Colors.deepPurple,
+          ));
+        }
       },
     );
   }
